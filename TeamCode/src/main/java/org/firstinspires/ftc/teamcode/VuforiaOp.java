39fa4fa23187599f42c8d01d5805ea8b6a4a773e
ElliottP-13 <pryorelliott@gmail.com>
2017-03-08 16:32:15 -0700

    Basic Vuforia
    Need to add robot position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
@@ -15,8 +20,37 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 /**
  * Created by Elliott on 2/5/2017.
  */
-
+@TeleOp(name = "Vuforia", group = "Linear Opmode")
 public class VuforiaOp extends LinearOpMode {
+
+    float mmPerInch        = 25.4f;
+    float mmBotWidth       = 18 * mmPerInch;            // ... or whatever is right for your robot
+    float mmFTCFieldWidth  = (12*12 - 2) * mmPerInch;   // the FTC field is ~11'10" center-to-center of the glass panels
+
+    OpenGLMatrix redTargetLocationOnField = OpenGLMatrix
+                /* Then we translate the target off to the RED WALL. Our translation here
+                is a negative translation in X.*/
+            .translation(-mmFTCFieldWidth/2, 0, 0)
+            .multiplied(Orientation.getRotationMatrix(
+                        /* First, in the fixed (field) coordinate system, we rotate 90deg in X, then 90 in Z */
+                    AxesReference.EXTRINSIC, AxesOrder.XZX,
+                    AngleUnit.DEGREES, 90, 90, 0));
+
+    OpenGLMatrix blueTargetLocationOnField = OpenGLMatrix
+                /* Then we translate the target off to the Blue Audience wall.
+                Our translation here is a positive translation in Y.*/
+            .translation(0, mmFTCFieldWidth/2, 0)
+            .multiplied(Orientation.getRotationMatrix(
+                        /* First, in the fixed (field) coordinate system, we rotate 90deg in X */
+                    AxesReference.EXTRINSIC, AxesOrder.XZX,
+                    AngleUnit.DEGREES, 90, 0, 0));
+
+    OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix
+            .translation(mmBotWidth/2,0,0)
+            .multiplied(Orientation.getRotationMatrix(
+                    AxesReference.EXTRINSIC, AxesOrder.YZY,
+                    AngleUnit.DEGREES, -90, 0, 0));
+
     @Override
     public void runOpMode() throws InterruptedException {
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
@@ -33,20 +67,28 @@ public class VuforiaOp extends LinearOpMode {
         beacons.get(2).setName("Lego");
         beacons.get(3).setName("Gears");
 
+        for(VuforiaTrackable beac : beacons){
+            ((VuforiaTrackableDefaultListener)beac.getListener()).setPhoneInformation(phoneLocationOnRobot, params.cameraDirection);
+        }
+
         waitForStart();
 
         beacons.activate();//starts tracking
         while(opModeIsActive()){
             for(VuforiaTrackable beac : beacons){
                 OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
-
+                OpenGLMatrix robotPose = ((VuforiaTrackableDefaultListener)beac.getListener()).getUpdatedRobotLocation();
                 if(pose != null){//pose would be null if the object isn't found
                     VectorF translation = pose.getTranslation();//makes it so it doesn't return 0 for odd reason
+
                     telemetry.addData(beac.getName() + "-Translation", translation);
                     double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2)));//if vertical do .get(1), .get(2) (y, z)
                     //if horizontal do .get(0) .get(2) (x, z)
                     telemetry.addData(beac.getName() + " Degrees", degreesToTurn);
 
+                }
+                if(robotPose != null){
+                    telemetry.addData("Robot Location: ", robotPose.formatAsTransform());
                 }
             }
             telemetry.update();
